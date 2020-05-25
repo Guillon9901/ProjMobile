8d28aa2b4875e09dc07b8b60ea3e6856696417e1
Meegan <guillon@et.esiea.fr>
2020-05-25 11:09:32 +0200

    Add MVC architecture for MainScreen

--- app/src/main/java/com/example/projmobile/Constants.java
+++ app/src/main/java/com/example/projmobile/Constants.java
@@ -2,5 +2,7 @@ package com.example.projmobile;
 
 public class Constants {
 
+    public static final String BASE_URL = "https://pokeapi.co/";
+
     public static String KEY_POKEMON_LIST = "jsonPokemonList";
 }
